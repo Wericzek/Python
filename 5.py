faf87c2295158b49c1764bedd01d14cc963b195b
Weronika <39522383+Wericzek@users.noreply.github.com>
2018-12-04 15:37:11 +0100

    file tree

--- 5.py
+++ 5.py
@@ -1,7 +1,25 @@
 import os
 
-path = "/Users/plweopy/Desktop/python"
-print(os.listdir(path))
+import sys
 
-for file in os.listdir(path):
-	print(file)
+def FileTree(path, level):
+	for roots, dirs, files in os.walk(path):
+		deep = level * "    "
+
+		for dir in dirs:
+			sys.stdout.write(deep + "- " + dir + "\n\r")
+			path2 = path + "/" + dir
+			level2 = level + 1
+			FileTree(path2, level2)
+
+		for file in files:
+			sys.stdout.write(deep + ">" + file + "\n\r")
+
+		break;
+
+	return;
+
+
+mPath = "/Users/plweopy/Desktop/python"
+print("File tree: ", mPath, "\n\r")
+FileTree(mPath, 0)
